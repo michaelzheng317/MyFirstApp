0382370315b28814443c89674596966b5a31edf7
michaelzheng <michaelzheng317@gmail.com>
2013-09-14 01:23:19 -0400

    first commit

---
+++ src/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,47 @@
+package com.example.myfirstapp;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends Activity {
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        
+       
+        // Create the text view
+        TextView textView1 = (TextView) findViewById(R.id.textView1);
+        textView1.setTextSize(40);
+        textView1.setText(message);
+
+        // Set the text view as the activity layout
+        //setContentView(textView1);
+    
+        TextView textView2 = (TextView) findViewById(R.id.textView2);
+        textView2.setTextSize(40);
+        textView2.setText(message + "123");
+        //setContentView(textView2);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case android.R.id.home:
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
